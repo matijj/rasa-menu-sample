bb83c3e6c2ab98d10af5148a780b004dc40b127c
Gabriel Bartolazzi <gbartolazzi@bechsud.com>
2023-11-29 15:19:57 -0300

    Init repo

---
+++ README.md
@@ -0,0 +1,97 @@
+# Rasa Menu - Sample Bot
+
+This is an example of a bot that displays a menu and a submenu.
+
+In some channels, option buttons cannot be used. The goal is to show a menu with letters so that the user can navigate through different options. Additionally, the user can also type what they want to do.
+
+For example, the bot initially offers:
+      This is the main menu:
+        A. Info Enterprise
+        B. Turns
+        C. List People
+      Choose an option or type what you are looking for.
+
+Then the user can:
+ - Type one of the letters, for example, letter A.
+ - Or they can directly type the intention like "I want to know about the enterprise."
+
+Additionally, since there are times when there are many options, there is also the possibility of using a sub-menu.
+
+For example:
+The bot initially offers:
+      This is the main menu:
+        A. Info Enterprise
+        B. Turns
+        C. List People
+      Choose an option or type what you are looking for.
+
+The user can type:
+ - Type letter B.
+
+And they will be shown the submenu:
+      Please, select an option:
+        A. Request a new turn
+        B. List your turns
+        C. Delete a turn
+        D. Back to the main menu
+
+## How It Works
+The bot primarily relies on a central slot, known as current_menu, to keep track of the menu the user is currently navigating. When a user attempts to select an option (done through a regular expression that detects letters), the selected option is stored in the current_menu slot. This action is then processed by a custom action, which determines the active menu and the user's choice within that menu.
+
+Maintaining an up-to-date current_menu is crucial. It's activated when a user's intent is a specific menu selection. However, there's also an action in place to clear the slot if the user makes an invalid selection, such as entering a letter that doesn't correspond to any menu item.
+
+Additionally, the bot provides examples of calling actions or utterances as part of its functionality.
+
+This architecture ensures smooth navigation through menus, allowing users to interact with the bot effectively.
+
+## Install dependencies
+
+Run :
+```bash
+pip install -r requirements.txt
+```
+
+## Run the bot
+
+Use `rasa train` to train a model. It is also possible to generate training data for scenarios (stories) by using [interactive learning](https://rasa.com/docs/rasa/writing-stories/#using-interactive-learning) with the command:
+```
+rasa interactive -m {path_to_a_model} 
+```
+> If there is no model already trained, you can remove the argument. Rasa will train the model with actual data.
+
+
+Then, to run the bot, first set up your action server in one terminal window:
+```bash
+rasa run actions
+```
+
+In another window, you can talk to the bot by running:
+```
+rasa shell --debug  
+```
+
+It is also possible to do the two above with a single command:  
+```
+rasa run actions & rasa shell --debug  
+```
+
+> Note that `--debug` mode will produce a lot of output meant to help you understand how the bot is working under the hood. To simply talk to the bot, you can remove this flag.
+
+
+## Overview of the files
+
+`data/stories.yml` - contains [stories](https://rasa.com/docs/rasa/stories/)
+
+`data/rules.yml` - contains [rules](https://rasa.com/docs/rasa/rules)
+
+`data/nlu/` - contains [NLU training data](https://rasa.com/docs/rasa/nlu-training-data)
+
+`actions/actions.py` - contains [custom action](https://rasa.com/docs/rasa/custom-actions)/api calls code
+
+`domain.yml`         - the [domain](https://rasa.com/docs/rasa/domain) file, including bot response templates
+
+`config.yml`         - training [configurations](https://rasa.com/docs/rasa/model-configuration) for the NLU pipeline and policy ensemble
+
+## Contributing
+
+Feel free to make a PR or report an issue
---
+++ actions/actions.py
@@ -0,0 +1,117 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, FollowupAction
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHandleOptions(Action):
+    def name(self) -> Text:
+        return "action_handle_options"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        # The default value is main
+        current_menu = tracker.get_slot("current_menu")
+
+        # If the current menu is not set, we assume that the user is in the main menu
+        if current_menu is None:
+            dispatcher.utter_message(text=f"Mmmmm... I don't know what you mean.")
+            return [SlotSet("option", None)]
+
+        menuoptions_to_actionname = {
+            "main": {
+                "A": "utter_info_enterprise",
+                "B": ("utter_turn_options", "turn_options"),
+                "C": "action_list_people",
+            },
+            "turn_options": {
+                "A": "action_request_turn",
+                "B": "action_list_turns",
+                "C": "action_delete_turn",
+                "D": "utter_back_to_main_menu"
+            }
+        }
+        option = tracker.get_slot("option")
+        option = option.upper()
+
+        try:
+            next_action = menuoptions_to_actionname[current_menu][option]
+        except KeyError:
+            dispatcher.utter_message(text=f"This option is not available!")
+            return [SlotSet("option", None)]
+
+        dispatcher.utter_message(text=f"You've choosen option {option} !")
+
+        if type(next_action) is tuple:
+            return [SlotSet('option', None),
+                    SlotSet('current_menu', next_action[1]),
+                    FollowupAction(name=next_action[0])]
+        else:
+            return [SlotSet('option', None),
+                    SlotSet('current_menu', None),
+                    FollowupAction(name=next_action)]
+
+class ActionClearCurrentMenu(Action):
+    def name(self):
+        return "action_clear_current_menu"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict) -> List[Dict[Text, Any]]:
+        # Obtén la última intención del usuario
+        latest_intent = tracker.latest_message["intent"]["name"]
+
+        # Verifica si la última intención fue "give_option"
+        if latest_intent != "give_option":
+            # Si la intención no fue "give_option", establece current_menu en None
+            return [SlotSet("current_menu", None)]
+        else:
+            # Si la intención fue "give_option", no hagas nada con current_menu
+            return []
+
+class ActionListPeople(Action):
+    def name(self):
+        return "action_list_people"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+        dispatcher.utter_message(text="List of people")
+
+        return []
+
+class ActionRequestTurn(Action):
+    def name(self):
+        return "action_request_turn"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+        dispatcher.utter_message(text="Request Turn")
+
+        return []
+
+
+class ActionListTurns(Action):
+    def name(self):
+        return "action_list_turns"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+        dispatcher.utter_message(text="List Turns")
+
+        return []
+
+class ActionDeleteTurn(Action):
+    def name(self):
+        return "action_delete_turn"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+        dispatcher.utter_message(text="Delete Turn")
+
+        return []
